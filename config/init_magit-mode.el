8c1e968a42281cbfb2bf28fe1c524c708abeceff
itai <itaimain@gmail.com>
2016-02-12 21:27:41 +0200

    Configure magit
    Always vertical on ediff mode.

--- config/init_magit-mode.el
+++ config/init_magit-mode.el
@@ -1,1 +1,6 @@
 (global-set-key (kbd "C-M-m") 'magit-status)
+
+(custom-set-variables
+ '(ediff-window-setup-function 'ediff-setup-windows-plain)
+ '(ediff-diff-option "-w")
+ '(ediff-split-window-function 'split-window-horizontally))
