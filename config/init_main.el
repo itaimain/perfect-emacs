a555cd16641d54c4463a3412e20f6cc46fea76cd
itai <itaimain@gmail.com>
2016-02-13 04:37:57 +0200

    Enable emacs to run correctly in daemon mode
    As an emacsclient.

--- config/init_main.el
+++ config/init_main.el
@@ -3,18 +3,16 @@
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
 ;; Graphic configurations
-(defun fullscreen ()
-  (interactive)
-  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
-			 '(2 "_NET_WM_STATE_FULLSCREEN" 0)))
-(fullscreen)
+(add-to-list 'default-frame-alist '(fullscreen . fullboth))
 (setq inhibit-startup-message t)
+(setq inhibit-x-resources 't)
 (load-theme 'wombat)
 (set-face-attribute 'region nil :background "#555" :foreground 'unspecified)
 (which-function-mode 1)
 (tool-bar-mode -1)
-(toggle-scroll-bar -1)
+(scroll-bar-mode -1)
 (menu-bar-mode -1)
+(blink-cursor-mode 1)
 
 ;; ---------------------------
 
@@ -53,6 +51,7 @@
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
 (global-linum-mode 1)
+(column-number-mode 1)
 (global-whitespace-cleanup-mode t)
 (global-set-key (kbd "C-c w") 'whitespace-mode)
 (require 'makefile-runner)
--- init.el
+++ init.el
@@ -1,5 +1,3 @@
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/config/"))
-
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.org/packages/") t)
